dfaadf6913ef868e910caf304ea8cafdd77e5210
Shahd <shahdelmahdy22@gmail.com>
2026-06-04 14:04:59 +0300

    remove

--- src/modules/payment/index.js
+++ src/modules/payment/index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { isAuthenticated } from "../../middleware/authentication.middleware.js";
+// import { isAuthenticated } from "../../middleware/authentication.middleware.js";
 
 // import { protect } from '../auth/auth.controller.js';
 import { payment } from "../../modules/payment/paymentController.js";
 
 const router = express.Router();
 
-router.use(isAuthenticated);
+// router.use(isAuthenticated);
 
 
 router.post("/", payment);
